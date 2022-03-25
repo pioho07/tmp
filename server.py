3ac0f8259ac59c72709d20a796f1b5725803b1ba
root <root@ip-172-31-6-68.ap-northeast-1.compute.internal>
2022-03-25 12:34:30 +0000

    server.py modify

--- server.py
+++ server.py
@@ -22,12 +22,6 @@ PORT = int(os.environ.get('PORT'))
 rds = boto3.client('rds')
 
 try:
-    token = rds.generate_db_auth_token(
-        DBHostname=DATABASE_HOST,
-        Port=DATABASE_PORT,
-        DBUsername=DATABASE_USER,
-        Region=DATABASE_REGION
-    )
     mydb =  mysql.connector.connect(
         host=DATABASE_HOST,
         user=DATABASE_USER,
